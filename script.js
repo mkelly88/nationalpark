e1dc79323649b637a659be66e3ded05678df742d
Madison <mkelly1081@gmail.com>
2021-07-30 22:26:53 -0500

    files updated

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ $(document).ready(() => {
           output += `
             <div class="col-md-3">
               <div class="well text-center">
-                <img src="${park.images[0].url}">
-                <h5>${park.fullName}</h5>
-                <a onclick="parkSelected('${park.id}')"class="btn btn-primary" href="#">Park Details</a>
+                <img src="${park.images[0].url}"> 
+                <h5 class="imgName">${park.fullName}</h5>
+                <a onclick="parkSelected('${park.parkCode}')"class="parkBtn" href="park.html">Park Details</a>
               </div>
             </div>
           `;
@@ -37,26 +37,43 @@ $(document).ready(() => {
         console.log(err);
     })}
 
-    function parkSelected(id){
-      sessionStorage.setItem('parkId', id);
-      window.location = 'park.html';
-      return false;
+   
+  function parkSelected(code){
+    sessionStorage.setItem('parkCode', code);
+    console.log(parkCode);
+    window.location = 'park.html';
+    return false;
   }
 
-  function getPark(){
-    let parkId = sessionStorage.getItem('parkId');
-    console.log(parkId)
 
-    axios.get('https://developer.nps.gov/api/v1/parks?i='+parkId+'&api_key=z5aTTxKjyFgjnbJSRxYfFYEXkXIgKSA9fkN4nK4P')
+  function getPark(){
+    let parkCode = sessionStorage.getItem('parkCode');
+    console.log(parkCode)
+
+    axios.get('https://developer.nps.gov/api/v1/parks?parkCode='+parkCode+'&api_key=z5aTTxKjyFgjnbJSRxYfFYEXkXIgKSA9fkN4nK4P')
     .then((response) => {
-      console.log(response.data);
-      let park = response.data;
+      console.log(response.data.data);
+      let park = response.data.data;
+      console.log(park);
 
       let output =`
+      <div class="parkDetails">
        <div class="row">
-       <h5 class="parkName">${park.name}
+        <img src="${park.images[1].url}">
+        <h5 class="parkName">${park.fullName}</h5>
        </div>
+        <div class="contact row">
+         <h2> ${park.adresses[0].url} </h2>
+        </div>
+      </div>
          `;
 
          $('#park').html(output);
-    })}
+       
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  }
